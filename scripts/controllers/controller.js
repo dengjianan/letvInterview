068315e613520c0141e0778f6aee2d7b423878e4
deng <onlydengjianan@126.com>
2015-12-29 23:29:37 +0800

    本次提交jsonp跨域方式的实现

--- scripts/controllers/controller.js
+++ scripts/controllers/controller.js
@@ -2,12 +2,22 @@ var img = myId('img');
 var test = myId('child1');
 var parent = myId('parent');
 var API = 'http://localhost:9001/sources/thanks.json';
+var API2 = 'http://localhost:8080/api/articles'
 var newP1 = document.createElement('p');
 newP1.innerHTML = "background:red";
 var newbtn = document.createElement('button');
 newbtn.innerHTML = 'get data';
 newbtn.onclick = function(){
-	getData(API);
+	// $.ajax('GET',API,null,function(data){
+	// 	console.log(data);
+	// })
+	// $.get(API2,function(data){
+	// 	console.log(data);
+	// })
+	ajaxByJsonp (API2);
+}
+function callbackHandler(data){
+	console.log(data)
 }
 img.onclick = function(){
 	parent.insertBefore(newP1,test);
--- scripts/services/services.js
+++ scripts/services/services.js
@@ -1,20 +1,19 @@
-function getData(url){//without cros
-	var xhr;
-	try{
-		xhr = new XMLHttpRequest();
-	}catch(e){
-		xhr = new ActiveXObject('Microsoft.XMLHttp');
-	}
-	xhr.onreadaystatechange = function(){
-		if(xhr.readyState == 4){
-			if(xhr.status >= 200&&xhr.status <= 300||xhr.status==304 ){
-				//deal data 
-				data = xhr.responseText;
-				console.log('123');
-			}
+var $ = {};
+$.get = function(url,fn){
+	 this.ajax('GET',url,null,fn);
+}
+$.ajax = function(ty,url,data,fn){
+	var xmlHttp = window.ActiveXObject?new ActiveXObject('Microsoft.XMLHttp'):new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function(){
+		if(xmlHttp.readyState == 4&&xmlHttp.status==200){
+			fn(xmlHttp.responseText);
 		}
 	}
-	xhr.open('GET',url,true);
-	xhr.send();
-	console.log(data);
+	xmlHttp.open(ty,url,true);
+	xmlHttp.send(data);
+}
+var ajaxByJsonp =function(url){
+	var script = document.createElement('script');
+	script.setAttribute('src',url+'?callback=callbackHandler');
+	document.getElementsByTagName('head')[0].appendChild(script);
 }
